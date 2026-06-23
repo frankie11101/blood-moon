d74ef9f2ef4a837a8b547cf1f2d8800255b10299
frankie11101 <frankieomts@gmail.com>
2026-06-23 15:17:28 -0300

    east wing rooms corridor cameras

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,9 +4,14 @@ extends Node3D
 @onready var classroom_camera12a: Camera3D = $"Classroom Camera 12A"
 @onready var classroom_camera11b: Camera3D = $"Classroom Camera 11B"
 @onready var classroom_camera11a: Camera3D = $"Classroom Camera 11A"
+@onready var classroom_camera_ew1: Camera3D = $"Classroom Camera EW1"
+@onready var classroom_camera_ew2: Camera3D = $"Classroom Camera EW2"
+@onready var classroom_camera_ew3: Camera3D = $"Classroom Camera EW3"
+@onready var classroom_camera_ew4: Camera3D = $"Classroom Camera EW4"
 @onready var corridorb_camerab: Camera3D = $"CorridorB Camera B"
 @onready var corridorb_cameraa: Camera3D = $"CorridorB Camera A"
 @onready var corridorc_cameraa: Camera3D = $"CorridorC Camera A"
+@onready var corridorc_camerab: Camera3D = $"CorridorC Camera B"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -68,5 +73,40 @@ func _on_corridor_area_a_4_body_entered(body: Node3D) -> void:
 
 
 func _on_corridor_c_area_a_1_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		corridorc_cameraa.make_current()
+
+
+func _on_classroom_area_ew_1_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		classroom_camera_ew1.make_current()
+
+
+func _on_classroom_area_ew_2_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		classroom_camera_ew2.make_current()
+
+
+func _on_classroom_area_ew_3_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		classroom_camera_ew3.make_current()
+
+
+func _on_classroom_area_ew_4_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		classroom_camera_ew4.make_current()
+
+
+func _on_corridor_c_area_a_4_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		corridorc_camerab.make_current()
+
+
+func _on_corridor_c_area_a_3_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		corridorc_camerab.make_current()
+
+
+func _on_corridor_c_area_a_2_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		corridorc_cameraa.make_current()
